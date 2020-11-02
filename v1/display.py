f19d63802e050610cc4467be521d3b3e29f0c275
Adam <adamr14@vt.edu>
2020-11-02 16:07:16 -0500

    Started display

--- v1/display.py
+++ v1/display.py
@@ -21,7 +21,6 @@ try:
     epd = epd7in5_V2.EPD()
     logging.info("init and Clear")
     epd.init()
-    epd.Clear()
 
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
@@ -30,34 +29,15 @@ try:
     logging.info("2.Drawing on the Vertical image...")
     Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Limage)
-    draw.text((2, 0), 'Goodmorning Adam', font = font18, fill = 0)
-    draw.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-    draw.line((10, 90, 60, 140), fill = 0)
-    draw.line((60, 90, 10, 140), fill = 0)
-    draw.rectangle((10, 90, 60, 140), outline = 0)
-    draw.line((95, 90, 95, 140), fill = 0)
-    draw.line((70, 115, 120, 115), fill = 0)
-    draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw.rectangle((10, 150, 60, 200), fill = 0)
-    draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
+    draw.text((90, 70), 'Date', font = font24, fill = 0)
+    draw.text((325, 70), 'Weather', font = font24, fill = 0)
+    draw.text((20, 140), 'Todo', font = font24, fill = 0)
+    
+    # Dividers
+    draw.line((0, 120, 480, 120),width = 4, fill = 0)
+    draw.line((300, 0, 300, 120),width = 4, fill = 0)
     epd.display(epd.getbuffer(Limage))
     time.sleep(2)
-
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
-    time.sleep(2)
-
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
 
     logging.info("Goto Sleep...")
     epd.sleep()
@@ -68,5 +48,5 @@ except IOError as e:
     
 except KeyboardInterrupt:    
     logging.info("ctrl + c:")
-    epd7in5.epdconfig.module_exit()
+    #epd7in5.epdconfig.module_exit()
     exit()
